9aaaf2b072a6755ada17da8593587147861e1730
Alexander Kremenets <kremenets0207@gmail.com>
2016-02-26 01:20:49 +0300

    New calculations, markup

--- js/index.js
+++ js/index.js
@@ -8,8 +8,71 @@ angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 })
 .controller('AppCtrl', function($scope) {
 
-  $scope.value1 = 0;
-  $scope.value2 = 0;
+  $scope.old = {
+    inputs: {
+      totalArea: {
+        name: 'Total office area',
+        value: 33000,
+      },
+      rent: {
+        name: 'Rent per sq. m. per year',
+        value: 20,
+      },
+      expense: {
+        name: 'Operating Expense per sq. m. per year',
+        value: 58.3333333,
+      }
+    },
+    formulas: {
+      monthlyRent: function monthlyRent() {
+        var i = $scope.old.inputs;
+
+        var result = i.totalArea.value * i.rent.value / 12 + i.expense.value / 12 + 16.66;
+        $scope.old.results.monthlyRent = result;
+        return result.toFixed(2);
+      }
+    },
+    results: {},
+  }
+
+  $scope.new = {
+    constants: {
+
+    },
+    inputs: {
+      office: {
+        name: 'Office usage',
+        value: 0,
+      },
+      meetings: {
+        name: 'Meetings usage',
+        value: 0,
+      },
+      kitchen: {
+        name: 'Kitchen usage',
+        value: 0,
+      },
+      storage: {
+        name: 'Storage usage',
+        value: 0,
+      },
+      print: {
+        name: 'Print usage',
+        value: 0,
+      }
+    },
+    formulas: {
+      monthlyRent: function monthlyRent() {
+        var i = $scope.new.inputs;
+        var oldRent = $scope.old.results.monthlyRent;
+
+        var result = oldRent / 100 * (0.759 * i.office.value + 0.167 * i.meetings.value + 0.26 * i.kitchen.value + 0.027 * i.storage.value + 0.021 * i.print.value);
+        $scope.new.results.monthlyRent = result;
+        return result.toFixed(2);
+      }
+    },
+    results: {},
+  }
 
 });
 
